6aa76a6113fcf2ef3816c91f23c879f4c0470bb5
Codevian Developer <developer.codevian@gmail.com>
2021-03-19 12:21:00 +0530

    Updates

---
+++ precache-manifest.98b3e4581656d39933c6348e04b09210.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f724e3a61c21c262b405f956abf6f0",
+    "url": "/typescript/index.html"
+  },
+  {
+    "revision": "94ab3b2c77b8691b1aa3",
+    "url": "/typescript/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "fc387482eea2103e88f9",
+    "url": "/typescript/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "94ab3b2c77b8691b1aa3",
+    "url": "/typescript/static/js/2.abf174b7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/typescript/static/js/2.abf174b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc387482eea2103e88f9",
+    "url": "/typescript/static/js/main.eb1733fc.chunk.js"
+  },
+  {
+    "revision": "25d611f661edf9c753ca",
+    "url": "/typescript/static/js/runtime-main.218ce773.js"
+  }
+]);
